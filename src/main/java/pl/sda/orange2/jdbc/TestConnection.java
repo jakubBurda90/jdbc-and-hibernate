81671110fe2c492a981ec7fb0e5271891f109897
jakubburda <jakub.burda90@gmail.com>
2023-03-05 15:59:04 +0100

    [add] - connection test

--- src/main/java/pl/sda/orange2/jdbc/TestConnection.java
+++ src/main/java/pl/sda/orange2/jdbc/TestConnection.java
@@ -3,16 +3,29 @@ package pl.sda.orange2.jdbc;
 import pl.sda.orange2.jdbc.config.H2config;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestConnection {
     public static void main(String[] args) {
+        String query = """
+                SELECT ID, NAME
+                FROM TEST
+                """;
+
 
         try{
 
            var h2connection = DriverManager.getConnection(H2config.DB_URL,H2config.USER,
                     H2config.PASSWORD);
             System.out.println("got connection " +(h2connection != null));
+
+            Statement queryStatement = h2connection.createStatement();
+            ResultSet queryResult = queryStatement.executeQuery(query);
+            queryResult.next();
+            System.out.println("ID: " +queryResult.getInt(1));
+            System.out.println("NAME: " +queryResult.getString(1));
         }catch (SQLException exc){
             System.out.println("got exception " + exc);
 
