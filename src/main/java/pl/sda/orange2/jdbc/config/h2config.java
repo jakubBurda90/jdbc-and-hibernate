0ce4743e2b763440fd48d552cc9dd90e0b28b96e
jakubburda <jakub.burda90@gmail.com>
2023-03-05 14:50:21 +0100

    [add] - jdbc package 2

--- src/main/java/pl/sda/orange2/jdbc/config/h2config.java
+++ src/main/java/pl/sda/orange2/jdbc/config/h2config.java
@@ -2,8 +2,8 @@ package pl.sda.orange2.jdbc.config;
 
 public class h2config {
 
-    private final static String DB_URL = "jdbc:h2:~/databases/jdb";
-    private final static String USER = "sa";
-    private final static String PASSWORD = "sa";
+    public final static String DB_URL = "jdbc:h2:~/databases/jdb";
+    public final static String USER = "sa";
+    public final static String PASSWORD = "sa";
 
 }
